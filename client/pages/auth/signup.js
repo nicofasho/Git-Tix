db9e2499ddddca1d1aa735bc31a0b5d842b78d8e
Christian Watson <chris23w@gmail.com>
2023-02-05 21:46:00 -0600

    Handle signup from form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,13 +1,17 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    const response = await axios.post('/api/users/signup', {
+      email,
+      password,
+    });
   };
 
   return (
