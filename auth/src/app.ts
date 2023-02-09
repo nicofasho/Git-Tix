56affefcddd059e1d293a589b2e14719408d281e
Christian Watson <chris23w@gmail.com>
2023-02-08 18:04:34 -0600

    Update auth app using common module

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import mongoose from 'mongoose';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@nicotickets/common';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true);
@@ -25,6 +23,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
